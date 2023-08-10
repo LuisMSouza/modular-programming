c0d3a08e94c4778d422309acedfe74a08f72d039
LuisMSouza <l.miguel-souza@hotmail.com>
2023-08-10 00:29:38 -0300

    Added DataReader module

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import modules.DataReader;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] vetor = DataReader.readInt("Teste reader: ", 5);
+        for (int number : vetor) {
+            System.out.println(number);
+        }
     }
 }
---
+++ src/modules/DataReader.java
@@ -0,0 +1,40 @@
+package modules;
+
+import java.util.Scanner;
+
+public class DataReader {
+    public static Scanner scan = new Scanner(System.in);
+
+    public static int[] readInt(String message, int quantity) {
+        /*
+         * Used to read and return numbers
+         * Params:
+         * message: Passed message that will be displayed before reading the number
+         * quantity: Number of numbers to be read
+         */
+        if (quantity <= 0)
+            quantity = 1;
+
+        int[] numbers = new int[quantity];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(message);
+            numbers[i] = scan.nextInt();
+        }
+        System.out.println(quantity + " numbers read!");
+
+        scan.close();
+        return numbers;
+    }
+
+    public static String readStr(String message) {
+        /*
+         * Used to read and return one string
+         * Params:
+         * message: Passed message that will be displayed before reading the number
+         */
+        String text = scan.nextLine();
+
+        return text;
+    }
+}
